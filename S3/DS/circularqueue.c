ff2682f1976ead91bdb7c8e0c3f9c069274d8f5a
Anurag <anuragrajanp@gmail.com>
2022-11-03 15:44:37 +0530

    Update circularqueue.c

--- S3/DS/circularqueue.c
+++ S3/DS/circularqueue.c
@@ -4,3 +4,75 @@
 #include<stdio.h>
 
 define n 5
+
+int queue[n], front = -1; rear = -1; data;
+
+void enqueue();
+void dequeue();
+void display();
+
+void main() {
+	int a;
+	while (a != 6) {
+		printf("\n 1. Enqueue \n 2. Dequeue \n 3. Display \n 4. Exit \n Enter your choice: ");
+		scanf("&d", &a);
+		switch (a) {
+		case 1: enqueue(); display(); break;
+		case 2: dequeue(); display(); break;
+		case 3: display(); break;
+		case 4: exit(-1);
+		default: printf("\n Enter valid choice!"); break;
+		}
+	}
+}
+
+void enqueue() {
+	if ((front = -1 && rear = -1) || (front != 0 && front == rear + 1)) {
+		printf("\n Queue overflow!");
+		exit(-1);
+	} else if (rear == (n-1) && front != 0) {
+		rear = 0;
+		printf("Enter data: ");
+		scanf("%d", &data);
+		queue[rear] = data;
+	} else {
+		rear = rear++;
+		printf("Enter data: ");
+		scanf("%d", &data);
+	}
+}
+
+void dequeue() {
+	if (front == -1 && rear == -1) {
+		printf(" \n Queue is underflow!");
+		return;
+	} else if (front == rear) {
+		printf("\n Your element is %d", queue[rear]);
+		front = -1;
+		rear = -1;
+	} else {
+		if (front = n - 1)
+			front = 0;
+		else {
+			front = front + 1;
+		}
+	}
+}
+
+void display() {
+	if (front == -1 && rear == -1) {
+		printf("\n Queue empty!");
+		return;
+	} else if (front <= rear) {
+		printf("\n %d", queue[i]);
+		printf("\n");
+	} else {
+		printf("\n Your queue is: ");
+		for (int i = front; i <= n-1; i++)
+			printf ("%d", queue[i]);
+		}
+		
+		for (i = 0; i <= rear; i++)
+			printf ("%d", queue[i]);
+		printf ("\n");
+}
