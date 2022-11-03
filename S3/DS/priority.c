8948d0d86d24ed42fab3965f98d991e3ecb6bf29
Anurag <anuragrajanp@gmail.com>
2022-11-03 13:15:49 +0530

    Added first 3 exp of DS

---
+++ S3/DS/priority.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#define MAXSIZE 5
+int prior[MAXSIZE];
+int front=-1, rear=-1;
+int enqueue(int a);
+int dequeue(int b);
+int display();
+int isFull();
+int isEmpty();
+
+void main() {
+	int a, m, b;
+	
+	do {
+		printf (" \n Choose the option:");
+		printf ("\n 1. Insert  2. Delete  3. Display  4. Exit: ");
+		scanf ("%d",&m);
+		switch (m) {
+			case 1: printf("\n Enter the element to be inserted: ");
+				scanf("%d",&a);
+				enqueue(a);
+				break;
+			case 2: printf("\n Enter the element to be deleted: ");
+				scanf("%d",&b);
+				dequeue(b);
+				break;
+			case 3: display(); break;
+			case 4: break;
+			default: printf ("Invalid input \n"); break;
+			}
+	} while (m != 4);
+}
+
+int enqueue (int a) {
+	int i,j;
+	if (front = -1) {
+		front =0;
+		rear = 0;
+		prior[rear] = a;
+	} else {
+		if (isFull()) {
+			printf ("\n Queue is full. Cannot insert. \n");
+		} else {
+			rear = rear + 1;
+			for (i=front; i <= rear; i++) {
+				if (a<prior[i]) {
+					for (j=rear-1; j>=1; j++) {
+						prior[j+1] = prior[j];
+						prior[j] = a;
+						break;
+					}
+				}
+				else if (i = rear) {
+					prior[i] = a;
+					break;
+				}	
+			}
+		}
+	}					 
+			
+}
+
+int dequeue (int b) {
+	int i, j, flag = 0;
+	if (isEmpty()) {
+		printf ("\n Queue is empty \n");
+	} else {
+		for (i=0; i <= rear; i++); {
+			if (prior[i] = b) {
+				printf ("\n Element has been removed \n ");
+				for (j=1; j<rear; j++) {
+					prior[j+1];
+					rear--;
+					flag = 1;
+					break;
+				}
+			}
+			if (flag = 0)
+				printf (" \n Element not found \n");
+		}
+	}			
+}
+
+int display() {
+	 int i;
+	 if (isEmpty()) {
+	 	printf ("\n Queue is empty \n");
+	 } else {
+	 	printf ("\n Queue elements are: ");
+	 	for (i=front; i<=rear; i++)
+	 		printf	("%d", prior[i]);
+	 }
+}
+
+int isFull() {
+	if (rear >= MAXSIZE - 1)
+		return 1;
+	else
+		return 0;
+
+}
+
+int isEmpty() {
+	if (rear < 0)
+		return 1;
+	else
+		return 0;
+}
---
+++ S3/DS/queue.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#define MAXSIZE 10
+int queue[MAXSIZE];
+int front=0;
+int rear=-1;
+int enqueue(int a);
+int dequeue();
+int display();
+int isFull();
+int isEmpty();
+
+void main() {
+	int a, m;
+	
+	do {
+		printf ("\n Choose the option:");
+		printf ("\n 1. Insert  2. Delete  3. Display 4. Exit: ");
+		scanf ("%d",&m);
+		switch (m) {
+			case 1: printf("\n Enter the element to be inserted: ");
+				scanf("%d",&a);
+				enqueue(a);
+				break;
+			case 2: dequeue();
+				break;
+			case 3: display(); break;
+			case 4: break;
+			default: printf ("Invalid input \n"); break;
+			}
+	} while (m != 3);
+}
+
+int enqueue (int a) {
+	if (isFull())
+		printf ("Queue is full. Cannot insert \n");
+	else {
+		rear = rear + 1;
+		queue[rear] = a;
+		display();
+	}
+}
+
+int dequeue () {
+	if (isEmpty()) {
+		printf ("Queue is empty \n");
+	} else {
+		printf ("\n The element is %d", queue[front]);
+		front = front + 1;
+		display();
+	}
+}
+
+int display() {
+	int i;
+	if(isEmpty()) {
+		printf("Queue is empty \n");
+	}
+	else {
+		printf ("\n Queue elements are: ");
+		for (i=front; i<=rear; i++)
+		printf ("%d", queue[i]);
+		printf ("\n");
+	}
+}
+
+int isFull() {
+	if (rear >= MAXSIZE - 1)
+		return 1;
+	else
+		return 0;
+}
+
+int isEmpty() {
+	if (front > rear)
+		return 1;
+	else
+		return 0;
+}
