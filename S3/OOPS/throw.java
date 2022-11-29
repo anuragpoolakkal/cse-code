1c048c7cbb590a61d29e991461cf20d48a33fb55
Anurag <anuragrajanp@gmail.com>
2022-11-29 15:04:10 +0530

    Create throw.java

---
+++ S3/OOPS/throw.java
@@ -0,0 +1,16 @@
+class throw {
+	Static void divide() throw Arithmetic exception  {
+		int s = 22, y = 0, z;
+		z = x / y;
+	}
+	
+	public static void main (Strigargs[]) {
+		try {
+			divide();
+		} catch (ArithmeticException e) {
+			System.out.println("Caught the eception  " + e;
+		} finally {
+		System.out.println("I'm always here!");
+		}
+	}
+}
