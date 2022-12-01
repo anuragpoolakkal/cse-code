ddf9fcb7c1d8d4a73e0905a0af89b0fb2474abdf
Anurag <anuragrajanp@gmail.com>
2022-12-01 13:47:28 +0530

    Update circularqueue.c

--- S3/DS/circularqueue.c
+++ S3/DS/circularqueue.c
@@ -10,84 +10,77 @@ void enqueue();
 void dequeue();
 void display();
 
-void main(){
+void main() {
 	int a = 0;
-	while(a != 4){
-		printf("1. Enqueue\n2. Dequeue\n3. Display\n4. Exit\n");
+	while (a != 4) {
+		printf("\n1. Enqueue\n2. Dequeue\n3. Display\n4. Exit\n");
 		printf("Enter your choice: ");
 		
 		scanf("%d", &a);
-		switch(a){
-			case 1:
-				enqueue();
+		switch (a) {
+			case 1: enqueue(); display();
 				break;
-			case 2:
-				dequeue();
+			case 2:	dequeue(); display();
 				break;
-			case 3:
-				display();
+			case 3:	display();
 				break;
-			case 4:
-				exit(-1);
-			default:
-				printf("Enter a valid choice");
+			case 4:	exit(-1);
+			default: printf("Enter a valid choice! \n");
 		}
 	}
 }
 
-void enqueue(){
-	if(front == -1 && rear == -1){
+void enqueue() {
+	if (front == -1 && rear == -1){
 		front = 0;
 	}
 	
-	if((front == 0) && (rear == (n - 1)) || (front != 0 && front == rear + 1)){
-		printf("Queue overflow");
-	}
-	else{
+	if ((front == 0) && (rear == (n - 1)) || (front != 0 && front == rear + 1)) {
+		printf("Queue overflow \n");
+	} else {
 		rear = (rear == n - 1 && front != 0) ? 0 : rear + 1;
 		printf("Enter data: ");
 		scanf("%d", &data);
 		queue[rear] = data;
-		printf("Data inserted!");
+		printf("Data inserted! \n");
 	}
 }
 
-void dequeue(){
-	if(front == -1 && rear == -1){
-		printf("Queue underflow");
+void dequeue() {
+	if (front == -1 && rear == -1) {
+		printf("Queue underflow \n");
 		return;
 	}
 	
 	
-	if(front == rear){
+	if (front == rear){
 		printf("Removed element is %d\n", queue[rear]);
 		front = rear = -1;	
-	}
-	else{
+	} else {
 		printf("Removed element is %d\n", queue[front]);
 		front = (front == n - 1) ? 0 : front + 1;
 	}
 }
 
-void display(){
-	if(front == -1 && rear == -1){
-		printf("Queue empty\n");
+void display() {
+	if (front == -1 && rear == -1){
+		printf("Queue empty \n");
 		return;
 	}
 	
 	int i;
-	printf("Your queue is: \n");
+	printf("Your queue is: ");
 		
-	if(front <= rear){
-		for(i = front; i <= rear; i++){
+	if (front <= rear) {
+		for (i = front; i <= rear; i++) {
 			printf("%d ", queue[i]);
 		}
 		printf("\n");
-	}else{
-		for(i = front; i <= n - 1; i++){
+	} else {
+		for (i = front; i <= n - 1; i++) {
 			printf("%d ", queue[i]);
 		}
-		for(i = 0; i <= rear; i++){
+		for (i = 0; i <= rear; i++) {
 			printf("%d ", queue[i]);
 		}
 		printf("\n");
