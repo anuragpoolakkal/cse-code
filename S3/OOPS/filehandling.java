ff1c372544e541f4d3af0809e6929fd1ebc2d375
Anurag <anuragrajanp@gmail.com>
2022-12-06 15:07:41 +0530

    Delete filehandling.java

--- S3/OOPS/filehandling.java
+++
@@ -1,9 +0,0 @@
-import java.io.FileWriter;
-import java.io.IOException;
-
-class createFile {
-    public static void main (String[] args) {
-        String str = "file handling in Java using" + " file writer and file reader";
-        FileWriter hv = new FileWriter("file.txt");
-    }
-}
