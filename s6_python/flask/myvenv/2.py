17b621bffd09bd5a0701c81ad71b7b77782bb525
Anurag <anuragrajanp@gmail.com>
2024-04-01 16:36:39 +0530

    tcp udp smtp ftp

---
+++ s6_python/flask/myvenv/2.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, redirect, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET',"POST"])
+def home():
+    if request.method == "POST":
+        num1 = request.form.get('   num1')
+        num2 = request.form.get('num2')
+        add = float(num1) + float(num2)
+        return render_template('result.html', add=add)
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8003)
